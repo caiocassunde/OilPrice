d99936a311b7c2dca4691e8936b0b2d46e3b913e
Caio Sabino <caio.cassunde@gmail.com>
2018-09-26 01:40:58 -0300

    Adequações de Código e Imagens

--- app/src/main/java/br/com/caiosabino/oilprice/MainActivity.java
+++ app/src/main/java/br/com/caiosabino/oilprice/MainActivity.java
@@ -1,40 +1,55 @@
 package br.com.caiosabino.oilprice;
 
-import android.support.design.widget.TextInputEditText;
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.lang.invoke.MethodHandleInfo;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
     private NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
     private TextView priceGasolineTextView;
     private TextView priceEthanolTextView;
-    //private TextView resultTextView;
+    private TextView resultTextView;
+    private ImageView oilImageView;
     private double priceGasoline = 2.5;
     private double priceEthanol = 2.5;
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         priceGasolineTextView = findViewById(R.id.priceGasolineTextView);
         priceEthanolTextView = findViewById(R.id.priceEthanolTextView);
-        //resultTextView = findViewById(R.id.resultTextView);
+        resultTextView = findViewById(R.id.resultTextView);
         priceGasolineTextView.setText(currencyFormat.format(priceGasoline));
         priceEthanolTextView.setText(currencyFormat.format(priceEthanol));
+        oilImageView = findViewById(R.id.oilImageView);
 
         final SeekBar gasolineSeekBar = findViewById(R.id.gasolineSeekBar);
         final SeekBar ethanolSeekBar = findViewById(R.id.ethanolSeekBar);
+
+        Dinamic dinamic = new Dinamic();
+        dinamic.changeImageAndText(priceEthanol, priceGasoline);
+
         gasolineSeekBar.setOnSeekBarChangeListener(
                 new SeekBar.OnSeekBarChangeListener() {
                     @Override
                     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                         priceGasoline = progress / 100.;
-                        String valorFormatadoGasoline = currencyFormat.format(priceGasoline);
-                        priceGasolineTextView.setText(valorFormatadoGasoline);
+                        String formattedValueGasoline = currencyFormat.format(priceGasoline);
+                        priceGasolineTextView.setText(formattedValueGasoline);
+                        Dinamic dinamic = new Dinamic();
+                        dinamic.changeImageAndText(priceEthanol, priceGasoline);
                     }
 
                     @Override
@@ -53,8 +68,10 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                         priceEthanol = progress / 100.;
-                        String valorFormatadoEthanol = currencyFormat.format(priceEthanol);
-                        priceEthanolTextView.setText(valorFormatadoEthanol);
+                        String formattedValueGasoline = currencyFormat.format(priceEthanol);
+                        priceEthanolTextView.setText(formattedValueGasoline);
+                        Dinamic dinamic = new Dinamic();
+                        dinamic.changeImageAndText(priceEthanol, priceGasoline);
                     }
 
                     @Override
@@ -68,8 +85,18 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
-        /*if(!(priceEthanol / priceGasoline >= 0.7)){
-            resultTextView.setText("Ethanol");
-        }*/
+    }
+
+    public class Dinamic {
+        public void changeImageAndText(double priceEthanol, double priceGasoline){
+            if((priceEthanol / priceGasoline >= 0.7)) {
+                oilImageView.setImageResource(R.drawable.gasolineImage);
+                resultTextView.setText(R.string.gasoline);
+            }
+            else{
+                oilImageView.setImageResource(R.drawable.ethanolImage);
+                resultTextView.setText(R.string.ethanol);
+            }
+        }
     }
 }
